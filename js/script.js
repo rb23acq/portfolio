53ffc73867de410a123897888bc6a1c665753ad0
rb23acq <rb23acq@herts.ac.uk>
2024-12-11 04:27:59 +0000

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,16 @@
+window.onload = function() {
+    if (window.location.pathname === '/' || window.location.pathname === '/index.html') {
+        alert("Hi, Welcome to my website!");
+    }
+};
+
+//selecting all of the images from the clickable class
+const images = document.querySelectorAll('.clickable-image');
+
+//loop
+images.forEach((image) => {
+    image.addEventListener('click', () => {
+// Toggle the 'enlarged' class
+        image.classList.toggle('enlarged');
+    });
+});

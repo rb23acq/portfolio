cae128e344f2d65971a468fc2a82fbe21f74ddbe
rb23acq <rb23acq@herts.ac.uk>
2024-12-11 03:49:07 +0000

    all files updated and ready to deploy

--- your-accessible-and-usable-profile-site-rb23acq/js/script.js
+++ your-accessible-and-usable-profile-site-rb23acq/js/script.js
@@ -1,14 +1,16 @@
-
-function myFunction() {
-    var x = document.getElementsByClassName("topnav")[0];
-    if (x.className === "topnav") {
-        x.className += " responsive";
-    } else {
-        x.className = "topnav";
+window.onload = function() {
+    if (window.location.pathname === '/' || window.location.pathname === '/index.html') {
+        alert("Hi, Welcome to my website!");
     }
-}
+};
 
-document.getElementById('btnAbout').addEventListener('click', function(event) {
-    event.preventDefault(); // Prevent the default anchor click behavior
-    document.getElementById('btnAbout').scrollIntoView({ behavior: 'smooth' }); // Scroll to the section smoothly
+//selecting all of the images from the clickable class
+const images = document.querySelectorAll('.clickable-image');
+
+//loop
+images.forEach((image) => {
+    image.addEventListener('click', () => {
+// Toggle the 'enlarged' class
+        image.classList.toggle('enlarged');
+    });
 });
